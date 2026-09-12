043778d830a4eec6de89cc640ba21692a8701294
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2026-09-11 21:04:29 -0300

    Solving Windows problem of order of things in memory check of tests

--- tests/src/memory_check.h
+++ tests/src/memory_check.h
@@ -2,6 +2,7 @@
 #include <new>
 #include <cstdint>
 #include <unordered_map>
+#include <atomic>
 #include <mutex>
 
 struct __mem_track {
@@ -10,6 +11,7 @@ struct __mem_track {
         bool array;
     };
     std::unordered_map<uintptr_t, __data> mem_map;
+    std::atomic_bool is_enabled{false};
     std::recursive_mutex mtx;
 
     ~__mem_track() {
@@ -17,16 +19,19 @@ struct __mem_track {
         if (mem_map.size() > 0) {
             std::printf("[MEM] Memory problem: not all freed!\n");
             for(const auto& i : mem_map) {
-                std::printf("[MEM] - #%p: %zu in size, %s\n", i.first, i.second.amount, i.second.array ? "ARRAY" : "NON ARRAY");
+                std::printf("[MEM] - #%p: %zu in size, %s\n", (void*)i.first, i.second.amount, i.second.array ? "ARRAY" : "NON ARRAY");
             }
         }
     }
 
     void add(void* ptr, size_t len, bool is_arr) {
+        if (!is_enabled) return;
         std::lock_guard<std::recursive_mutex> l(mtx);
         mem_map[(uintptr_t)ptr] = {len, is_arr};
     }
+
     void pop(void* ptr) {
+        if (!is_enabled) return;
         std::lock_guard<std::recursive_mutex> l(mtx);
         auto it = mem_map.find((uintptr_t)ptr);
         if (it != mem_map.end()) mem_map.erase(it); // there may be other allocation before including this
@@ -34,12 +39,27 @@ struct __mem_track {
 
     std::unordered_map<uintptr_t, __data> copy() {
         std::lock_guard<std::recursive_mutex> l(mtx);
-        std::unordered_map<uintptr_t, __data> cpy = mem_map;
-        return cpy;
+        return mem_map;
+    }
+
+    void enable() {
+        is_enabled = true;
+    }
+
+    void disable() {
+        is_enabled = false;
+    }
+    
+    static __mem_track& get_singleton() {
+        alignas(__mem_track) static char storage[sizeof(__mem_track)];
+        static __mem_track* ptr = nullptr;
+        if (!ptr) {
+            ptr = new (storage) __mem_track();
+        }
+        return *ptr;
     }
 };
 
-static inline __mem_track _mem;
 static thread_local bool _in_tracker = false;
 
 // Standard single-object allocation
@@ -48,7 +68,7 @@ inline void* operator new(std::size_t count) {
     if (void* ptr = std::malloc(count)) {
         if (!_in_tracker) {
             _in_tracker = true;
-            _mem.add(ptr, count, false);
+            __mem_track::get_singleton().add(ptr, count, false);
             _in_tracker = false;
         }
         return ptr;
@@ -62,7 +82,7 @@ inline void* operator new[](std::size_t count) {
     if (void* ptr = std::malloc(count)) {
         if (!_in_tracker) {
             _in_tracker = true;
-            _mem.add(ptr, count, true);
+            __mem_track::get_singleton().add(ptr, count, true);
             _in_tracker = false;
         }
         return ptr;
@@ -91,7 +111,7 @@ inline void operator delete(void* ptr) noexcept {
     if (!ptr) return;
     if (!_in_tracker) {
         _in_tracker = true;
-        _mem.pop(ptr);
+        __mem_track::get_singleton().pop(ptr);
         _in_tracker = false;
     }
     std::free(ptr);
@@ -101,7 +121,7 @@ inline void operator delete[](void* ptr) noexcept {
     if (!ptr) return;
     if (!_in_tracker) {
         _in_tracker = true;
-        _mem.pop(ptr);
+        __mem_track::get_singleton().pop(ptr);
         _in_tracker = false;
     }
     std::free(ptr);
@@ -117,4 +137,7 @@ inline void operator delete[](void* ptr, std::align_val_t) noexcept { ::operator
 inline void operator delete(void* ptr, std::size_t, std::align_val_t) noexcept { ::operator delete(ptr); }
 inline void operator delete[](void* ptr, std::size_t, std::align_val_t) noexcept { ::operator delete[](ptr); }
 
-auto mem_state() { return _mem.copy(); }
+auto mem_state() { return __mem_track::get_singleton().copy(); }
+
+auto enable_mem_check() { __mem_track::get_singleton().enable(); }
+auto disable_mem_check() { __mem_track::get_singleton().disable(); }
